e1868a332cc7400fece9cad00fb970a1b1d88183
maru501 <megatabunne422@gmail.com>
2025-06-29 20:03:25 +0900

    変更なし

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>つながるMy本棚</title>
+</head>
+<body>
+    <h1><span style="background-color:lightcyan">つながるMy本棚</span></h1>
+    <p>あなたの推し本をシェアしよう！</p>
+
+    <hr>
+
+    <p><a href="register.php">会員登録</a> or <a href="login.php">ログイン</a></p>
+    <ul>
+        <li><a href="bookshelf.php">My本棚</a></li>
+        <li><a href="timeline.php">つながる読書掲示板</a></li>
+    </ul>
+</body>
+</html>
