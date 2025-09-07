539cfe44b79d22237921e4548e6a8395f576cba3
maru501 <megatabunne422@gmail.com>
2025-09-07 20:07:25 +0900

    DB接続

--- m6-users.php
+++ m6-users.php
@@ -1,9 +1,6 @@
 <?php
 
-$dsn = 'mysql:dbname=***;host=localhost';
-$user = '***';
-$password = '***';
-$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+require_once "db.php";
 
 $sql="CREATE TABLE IF NOT EXISTS tbusers"
 ."("
